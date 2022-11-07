ccde45032863a84943bd4c4bcefec2790fa2cb87
한상우 <sktkddn777@naver.com>
2022-11-07 16:57:04 +0900

    Add: 영속성 관리 예제 3.13 테스트 코드 작성
    준영속 상태일 경우 merge에 관한 내용

---
+++ src/test/java/jpabook/jpabook/persistence/JunPersistenceTest.java
@@ -0,0 +1,63 @@
+package jpabook.jpabook.persistence;
+
+import jpabook.jpabook.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
+
+/**
+ * 예제 3.13 테스트
+ *
+ */
+@SpringBootTest
+public class JunPersistenceTest {
+
+    @PersistenceUnit
+    private EntityManagerFactory factory;
+
+
+    @Test
+    void 준영속_병합() {
+        Member member = createMember("memberA");
+        // 준영속 상태로 됨
+        member.setUsername("memberB");
+        mergeMember(member);
+    }
+
+    Member createMember(String username) {
+        EntityManager em1 = factory.createEntityManager();
+        EntityTransaction tx1 = em1.getTransaction();
+        tx1.begin();
+
+        Member member = new Member();
+        member.setUsername(username);
+
+        em1.persist(member);
+        tx1.commit();
+
+        em1.close();
+        return member;
+    }
+
+    void mergeMember(Member member) {
+        EntityManager em2 = factory.createEntityManager();
+        EntityTransaction tx2 = em2.getTransaction();
+
+        tx2.begin();
+        Member mergeMember = em2.merge(member);
+
+        tx2.commit();
+
+        System.out.println("준영속 상태 member = " + member.getUsername());
+        System.out.println("영속 상태 member = " + mergeMember.getUsername());
+
+        System.out.println("em2 contains member = " + em2.contains(member));
+        System.out.println("em2 contains mergeMember = " + em2.contains(mergeMember));
+
+        em2.close();
+    }
+}
