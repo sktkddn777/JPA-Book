7a288a957c131493988ac5508a686a53fa9e780e
한상우 <sktkddn777@naver.com>
2022-11-10 20:09:47 +0900

    Add: DDL 생성 기능 + 다양한 매핑 사용

--- src/main/java/jpabook/jpabook/domain/Member.java
+++ src/main/java/jpabook/jpabook/domain/Member.java
@@ -3,17 +3,35 @@ package jpabook.jpabook.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Data
-@Table(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE", // 제약 조건 명
+        columnNames = {"NAME", "AGE"}
+)})
 public class Member {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10)
     private String username;
+
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 }
+
