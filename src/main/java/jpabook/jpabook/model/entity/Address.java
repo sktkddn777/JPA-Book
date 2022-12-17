ea0aa215aef76e459406e390d1351adc8029eeef
한상우 <sktkddn777@naver.com>
2022-12-17 19:14:03 +0900

    add: chapter9 값 타입
    - Embedable 값 타입 적용하여 엔티티에 불필요한 부분 하나의 값으로 통일
    - city, zipcode, street -> address 로

---
+++ src/main/java/jpabook/jpabook/model/entity/Address.java
@@ -0,0 +1,14 @@
+package jpabook.jpabook.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Data
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
